569aa6bf1858a5ee6533249afa70ff96b51f9646
Nathaniel Henry <nat.henry7@gmail.com>
2026-01-15 16:43:11 -0800

    Test diagram for tagged facility features.

---
+++ generate_viz.js
@@ -0,0 +1,259 @@
+/*
+------------------------------------------------------------------------------->
+
+TITLE: Generate network diagram
+AUTHOR: Nat Henry, nat@henryspatialanalysis.com
+CREATED: January 15, 2026
+
+------------------------------------------------------------------------------->
+*/
+
+// Identify the div to be filled and create an SVG within it
+const div_id = '#network-chart';
+const district_hierarchy = {
+  "Central": ["Lilongwe", "Dowa", "Kasungu", "Nkhotakota"],
+  "Southern": ["Chikwawa", "Nsanje", "Mulanje"],
+}
+const div_height = d3.select(div_id).node().getBoundingClientRect().height,
+      div_width = d3.select(div_id).node().getBoundingClientRect().width;
+const svg = d3.select(div_id)
+  .append('svg:svg')
+    .attr('height', div_height)
+    .attr('width', div_width);
+
+
+// Setup ---------------------------------------------------------------------->
+
+// Set time for all transitions, in milliseconds
+const transition_time = 300;
+
+// Define a tooltip that will appear when mousing over a point
+var tooltip = d3.select(div_id)
+  .append("div")
+  .attr('class','tooltip')
+    .style('position', 'absolute')
+    .style('font', '12px sans-serif')
+    .style('line-height', 1.5)
+    .style('background', '#ddd')
+    .style('padding', '5px')
+    .style('padding-bottom', '10px')
+    .style('border', '0px')
+    .style('border-radius', '8px')
+    .style('opacity', 0.);
+
+// Function to define "opening" the tooltip
+function open_tooltip(d){
+  if(d.Region === null) return;
+
+  // Set up x marker (to indicate that the tooltip can be closed by clicking)
+  const x_style = "style='padding:0; margin:0 0 -5px 0; text-align:right; font:3px;'";
+  var inner_html = "<div " + x_style + ">x</div>";
+  // Set up styling for any titles
+  const h3_style = "style='text-align:center; margin:0 0 5px 0; padding:0;'";
+
+  inner_html += "<h3 " + h3_style + ">" + d.name + "</h3>" +
+                "<i>District:</i> " + d.District + "<br/>" +
+                "<i>Region:</i> " + d.Region + "<br/>";
+  tooltip.style('text-align', 'left');
+  tooltip
+    .html(inner_html)
+    .style('left', (d3.event.pageX) + 'px')
+    .style('top', (d3.event.pageY) + 'px')
+    .transition(transition_time)
+      .style('opacity', 0.9);
+}
+
+// Function to define "closing" the tooltip
+tooltip.on('click', function(_){
+  tooltip.style('opacity', 0.0);
+  tooltip.html('');
+});
+
+
+// Create the force-directed diagram ------------------------------------------>
+
+// Set color scheme
+// Color scheme inspired by Color Brewer: https://colorbrewer2.org/
+const region_names = ['Central', 'Southern'];
+const region_colors = ['#e41a1c', '#377eb8'];
+const col_scale = d3.scaleOrdinal(region_colors).domain(region_names);
+
+// Define the force-directed diagram
+const simulation = d3.forceSimulation(network_data.nodes)
+  .force("charge", d3.forceManyBody().strength(-200))
+  .force(
+    "link",
+    d3.forceLink(network_data.links)
+      .id(d => d.id)
+      .distance(30)
+      .strength(.2)
+    )
+  .force("center", d3.forceCenter(div_width / 2 + 30, div_height / 2 + 30))
+  .force("x", d3.forceX())
+  .force("y", d3.forceY());
+
+// Set the behavior for dragging a node
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.5).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
+
+// Visualize links between nodes
+const link = svg.append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+  .selectAll("line")
+    .data(network_data.links)
+    .join("line");
+
+// Visualize nodes as circles
+const node = svg.append("g")
+    .attr('class', 'node-point')
+    .attr("stroke-width", 1.)
+  .selectAll("circle")
+  .data(network_data.nodes)
+  .join("circle")
+    .attr("stroke", d => d.Region === null ? '#000' : '#AAA')
+    .attr("fill", d => d.Region === null ? '#000' : col_scale(d.Region))
+    .attr("r", d => d.Region === null ? 15 : 5)
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended))
+    .on('click', d => open_tooltip(d));
+
+var node_label = svg
+  .append('g')
+    .attr('class', 'node-label')
+  .selectAll('text')
+  .data(network_data.nodes)
+  .join('text')
+    .text(d => (d.Region === null) ? d.name : null)
+    .attr('x', d => (d.Region === null) ? d.x + 8 : d.x + 5)
+    .attr('y', d => (d.Region === null) ? d.y - 15 : d.y - 10)
+    .style('font', '10px sans-serif')
+    .style('fill', '#555')
+    .style('background-color', '#ffffff88')
+    .style('pointer-events', 'none');
+
+// Update points over time
+simulation.on("tick", () => {
+  link
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
+
+  node
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
+
+  node_label
+    .attr('x', d => (d.Region === null) ? d.x + 8 : d.x + 5)
+    .attr('y', d => (d.Region === null) ? d.y - 15 : d.y - 10);
+});
+
+
+// Add legend and highlighting buttons ---------------------------------------->
+
+// Create div that will store the legend and focus buttons
+var viz_guide = d3.select(div_id)
+  .append('div')
+    .style('position', 'absolute')
+    .style('top', '10px')
+    .style('left', '10px')
+    .style('margin', 0)
+    .style('padding', '5px')
+    .style('background', 'white')
+    .style('opacity', 0.85);
+
+// Add legend
+const legend_font_style = '10px sans-serif';
+
+var legend = viz_guide
+  .append('svg:svg')
+    .style('margin', 0)
+    .style('height', '40px')
+    .style('width', '110px')
+    .style('padding', 0);
+
+legend.selectAll('legend_dots')
+  .data(region_names)
+  .enter()
+  .append('circle')
+    .attr('cx', 10)
+    .attr('cy', function(_, i){ return(10 + i * 18) })
+    .attr('r', 5)
+    .style('fill', d => col_scale(d));
+legend.selectAll('legend_labels')
+  .data(region_names)
+  .enter()
+  .append('text')
+    .attr('x', 20)
+    .attr('y', function(_, i){ return(12 + i * 18) })
+    .style('fill', d => col_scale(d) )
+    .style('font', legend_font_style)
+    .text(d => d)
+    .attr('text-anchor', 'left')
+    .style('alignment-baseline', 'middle');
+
+// Function to reduce the opacity of all patron nodes where a particular
+//  condition is false
+function focus_grouping(district_name){
+  node
+    .transition(transition_time)
+    .style('opacity', d =>
+      d.Region === null ? 1.0 :
+      d.District == district_name ? 1.0 : 0.2
+    );
+}
+// Function to only show named entities in the NYT article
+function focus_named(){
+  node
+    .transition(transition_time)
+    .style('opacity', d =>
+      (d.data.display_name == "") ? 0.2 : 1.0
+    );
+}
+// Add focus buttons
+var focus_buttons = viz_guide
+  .append('div')
+    .style('font', legend_font_style)
+    .style('text-align', 'center')
+focus_buttons.append('h3')
+  .style('margin', '10px 0 5px 0')
+  .html('Focus on:')
+
+function add_focus_button(button_title, bg_color, focus_func){
+  focus_buttons
+    .append('button')
+    .style('font', legend_font_style)
+    .style('background-color', bg_color)
+    .style('width', '120px')
+    .style('height', '20px')
+    .style('padding', '0px')
+    .html(button_title)
+    .on('click', () => focus_func());
+  focus_buttons.append('br');
+}
+
+for(const region of Object.keys(district_hierarchy)){
+  for(const district of district_hierarchy[region]){
+    add_focus_button(district, col_scale(region), () => focus_grouping(district));
+  }
+}
+add_focus_button(
+  '<i>Clear focus</i>',
+  '#CCC',
+  () => node.transition(transition_time).style('opacity', 1.)
+);
---
+++ network_data.js
@@ -0,0 +1,796 @@
+const network_data = {
+  "nodes": [
+    {
+      "name": "Developing area",
+      "District": null,
+      "Region": null,
+      "id": 1
+    },
+    {
+      "name": "Flooding",
+      "District": null,
+      "Region": null,
+      "id": 2
+    },
+    {
+      "name": "High population",
+      "District": null,
+      "Region": null,
+      "id": 3
+    },
+    {
+      "name": "High viraemia",
+      "District": null,
+      "Region": null,
+      "id": 4
+    },
+    {
+      "name": "High-risk subpopulations",
+      "District": null,
+      "Region": null,
+      "id": 5
+    },
+    {
+      "name": "International border",
+      "District": null,
+      "Region": null,
+      "id": 6
+    },
+    {
+      "name": "Mobility",
+      "District": null,
+      "Region": null,
+      "id": 7
+    },
+    {
+      "name": "Poverty",
+      "District": null,
+      "Region": null,
+      "id": 8
+    },
+    {
+      "name": "Rapid population growth",
+      "District": null,
+      "Region": null,
+      "id": 9
+    },
+    {
+      "name": "Risky sexual behavior",
+      "District": null,
+      "Region": null,
+      "id": 10
+    },
+    {
+      "name": "Socioeconomic hub",
+      "District": null,
+      "Region": null,
+      "id": 11
+    },
+    {
+      "name": "Likuni Mission Hospital",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 12
+    },
+    {
+      "name": "Chinsapo (LLW City Assembly Clinic)",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 13
+    },
+    {
+      "name": "Area 25 Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 14
+    },
+    {
+      "name": "Mbabvi Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 15
+    },
+    {
+      "name": "Daeyang Luke Hospital",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 16
+    },
+    {
+      "name": "Mtenthera Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 17
+    },
+    {
+      "name": "Khongoni Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 18
+    },
+    {
+      "name": "Kang'oma Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 19
+    },
+    {
+      "name": "Matapila Health Centre",
+      "District": "Lilongwe",
+      "Region": "Central",
+      "id": 20
+    },
+    {
+      "name": "Mponela Rural Hospital",
+      "District": "Dowa",
+      "Region": "Central",
+      "id": 21
+    },
+    {
+      "name": "Chankhungu Health Centre",
+      "District": "Dowa",
+      "Region": "Central",
+      "id": 22
+    },
+    {
+      "name": "Dzaleka Health Centre",
+      "District": "Dowa",
+      "Region": "Central",
+      "id": 23
+    },
+    {
+      "name": "Madisi Mission Hospital",
+      "District": "Dowa",
+      "Region": "Central",
+      "id": 24
+    },
+    {
+      "name": "Dowa DHO",
+      "District": "Dowa",
+      "Region": "Central",
+      "id": 25
+    },
+    {
+      "name": "Kasungu DHO",
+      "District": "Kasungu",
+      "Region": "Central",
+      "id": 26
+    },
+    {
+      "name": "Kaluluma Rural Hospital",
+      "District": "Kasungu",
+      "Region": "Central",
+      "id": 27
+    },
+    {
+      "name": "Alinafe Community Hospital",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 28
+    },
+    {
+      "name": "Nkhunga Health Centre",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 29
+    },
+    {
+      "name": "Dwambazi",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 30
+    },
+    {
+      "name": "Ngala Health Centre",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 31
+    },
+    {
+      "name": "Nkhotakota DHO",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 32
+    },
+    {
+      "name": "Msenjere",
+      "District": "Nkhotakota",
+      "Region": "Central",
+      "id": 33
+    },
+    {
+      "name": "Lengwe Health Centre",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 34
+    },
+    {
+      "name": "St Montfort Mission Hospital",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 35
+    },
+    {
+      "name": "Ngabu Rural Hospital",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 36
+    },
+    {
+      "name": "Makhwira Health Centre",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 37
+    },
+    {
+      "name": "Chipwaila Health Centre",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 38
+    },
+    {
+      "name": "Chikwawa DHO",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 39
+    },
+    {
+      "name": "Ndakwera health Centre",
+      "District": "Chikwawa",
+      "Region": "Southern",
+      "id": 40
+    },
+    {
+      "name": "Trinity Mission Hospital",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 41
+    },
+    {
+      "name": "Kalemba Community Hospital",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 42
+    },
+    {
+      "name": "Mbenje Health Centre",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 43
+    },
+    {
+      "name": "Ndamera Health Centre",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 44
+    },
+    {
+      "name": "Tengani Health Centre",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 45
+    },
+    {
+      "name": "Nsanje DHO",
+      "District": "Nsanje",
+      "Region": "Southern",
+      "id": 46
+    },
+    {
+      "name": "Chonde Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 47
+    },
+    {
+      "name": "Thuchila Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 48
+    },
+    {
+      "name": "Chisitu Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 49
+    },
+    {
+      "name": "Mbiza Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 50
+    },
+    {
+      "name": "Milonde Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 51
+    },
+    {
+      "name": "Nkomoala Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 52
+    },
+    {
+      "name": "Muloza Health Centre",
+      "District": "Mulanje",
+      "Region": "Southern",
+      "id": 53
+    }
+  ],
+  "links": [
+    {
+      "source": 1,
+      "target": 12
+    },
+    {
+      "source": 1,
+      "target": 15
+    },
+    {
+      "source": 1,
+      "target": 18
+    },
+    {
+      "source": 1,
+      "target": 19
+    },
+    {
+      "source": 2,
+      "target": 37
+    },
+    {
+      "source": 2,
+      "target": 42
+    },
+    {
+      "source": 2,
+      "target": 52
+    },
+    {
+      "source": 2,
+      "target": 53
+    },
+    {
+      "source": 3,
+      "target": 12
+    },
+    {
+      "source": 3,
+      "target": 14
+    },
+    {
+      "source": 3,
+      "target": 19
+    },
+    {
+      "source": 3,
+      "target": 20
+    },
+    {
+      "source": 3,
+      "target": 35
+    },
+    {
+      "source": 3,
+      "target": 36
+    },
+    {
+      "source": 3,
+      "target": 37
+    },
+    {
+      "source": 3,
+      "target": 39
+    },
+    {
+      "source": 3,
+      "target": 46
+    },
+    {
+      "source": 3,
+      "target": 47
+    },
+    {
+      "source": 3,
+      "target": 50
+    },
+    {
+      "source": 4,
+      "target": 12
+    },
+    {
+      "source": 4,
+      "target": 13
+    },
+    {
+      "source": 4,
+      "target": 14
+    },
+    {
+      "source": 4,
+      "target": 15
+    },
+    {
+      "source": 4,
+      "target": 16
+    },
+    {
+      "source": 4,
+      "target": 19
+    },
+    {
+      "source": 4,
+      "target": 21
+    },
+    {
+      "source": 4,
+      "target": 22
+    },
+    {
+      "source": 4,
+      "target": 23
+    },
+    {
+      "source": 4,
+      "target": 24
+    },
+    {
+      "source": 4,
+      "target": 25
+    },
+    {
+      "source": 4,
+      "target": 26
+    },
+    {
+      "source": 4,
+      "target": 29
+    },
+    {
+      "source": 4,
+      "target": 30
+    },
+    {
+      "source": 4,
+      "target": 31
+    },
+    {
+      "source": 4,
+      "target": 32
+    },
+    {
+      "source": 4,
+      "target": 33
+    },
+    {
+      "source": 4,
+      "target": 34
+    },
+    {
+      "source": 4,
+      "target": 35
+    },
+    {
+      "source": 4,
+      "target": 36
+    },
+    {
+      "source": 4,
+      "target": 37
+    },
+    {
+      "source": 4,
+      "target": 38
+    },
+    {
+      "source": 4,
+      "target": 39
+    },
+    {
+      "source": 4,
+      "target": 42
+    },
+    {
+      "source": 4,
+      "target": 43
+    },
+    {
+      "source": 4,
+      "target": 44
+    },
+    {
+      "source": 4,
+      "target": 46
+    },
+    {
+      "source": 4,
+      "target": 47
+    },
+    {
+      "source": 4,
+      "target": 48
+    },
+    {
+      "source": 4,
+      "target": 49
+    },
+    {
+      "source": 4,
+      "target": 50
+    },
+    {
+      "source": 4,
+      "target": 51
+    },
+    {
+      "source": 4,
+      "target": 52
+    },
+    {
+      "source": 4,
+      "target": 53
+    },
+    {
+      "source": 5,
+      "target": 12
+    },
+    {
+      "source": 5,
+      "target": 13
+    },
+    {
+      "source": 5,
+      "target": 14
+    },
+    {
+      "source": 5,
+      "target": 16
+    },
+    {
+      "source": 5,
+      "target": 20
+    },
+    {
+      "source": 5,
+      "target": 21
+    },
+    {
+      "source": 5,
+      "target": 23
+    },
+    {
+      "source": 5,
+      "target": 25
+    },
+    {
+      "source": 5,
+      "target": 26
+    },
+    {
+      "source": 5,
+      "target": 28
+    },
+    {
+      "source": 5,
+      "target": 29
+    },
+    {
+      "source": 5,
+      "target": 30
+    },
+    {
+      "source": 5,
+      "target": 31
+    },
+    {
+      "source": 5,
+      "target": 34
+    },
+    {
+      "source": 5,
+      "target": 35
+    },
+    {
+      "source": 5,
+      "target": 36
+    },
+    {
+      "source": 5,
+      "target": 37
+    },
+    {
+      "source": 5,
+      "target": 41
+    },
+    {
+      "source": 5,
+      "target": 42
+    },
+    {
+      "source": 5,
+      "target": 46
+    },
+    {
+      "source": 5,
+      "target": 47
+    },
+    {
+      "source": 6,
+      "target": 36
+    },
+    {
+      "source": 6,
+      "target": 44
+    },
+    {
+      "source": 6,
+      "target": 45
+    },
+    {
+      "source": 6,
+      "target": 51
+    },
+    {
+      "source": 6,
+      "target": 53
+    },
+    {
+      "source": 7,
+      "target": 14
+    },
+    {
+      "source": 7,
+      "target": 26
+    },
+    {
+      "source": 7,
+      "target": 48
+    },
+    {
+      "source": 7,
+      "target": 52
+    },
+    {
+      "source": 8,
+      "target": 14
+    },
+    {
+      "source": 8,
+      "target": 16
+    },
+    {
+      "source": 8,
+      "target": 19
+    },
+    {
+      "source": 8,
+      "target": 36
+    },
+    {
+      "source": 8,
+      "target": 38
+    },
+    {
+      "source": 8,
+      "target": 52
+    },
+    {
+      "source": 9,
+      "target": 14
+    },
+    {
+      "source": 9,
+      "target": 39
+    },
+    {
+      "source": 9,
+      "target": 40
+    },
+    {
+      "source": 10,
+      "target": 13
+    },
+    {
+      "source": 10,
+      "target": 17
+    },
+    {
+      "source": 10,
+      "target": 29
+    },
+    {
+      "source": 10,
+      "target": 31
+    },
+    {
+      "source": 10,
+      "target": 35
+    },
+    {
+      "source": 10,
+      "target": 42
+    },
+    {
+      "source": 10,
+      "target": 43
+    },
+    {
+      "source": 10,
+      "target": 46
+    },
+    {
+      "source": 10,
+      "target": 48
+    },
+    {
+      "source": 10,
+      "target": 49
+    },
+    {
+      "source": 11,
+      "target": 12
+    },
+    {
+      "source": 11,
+      "target": 14
+    },
+    {
+      "source": 11,
+      "target": 16
+    },
+    {
+      "source": 11,
+      "target": 17
+    },
+    {
+      "source": 11,
+      "target": 21
+    },
+    {
+      "source": 11,
+      "target": 23
+    },
+    {
+      "source": 11,
+      "target": 24
+    },
+    {
+      "source": 11,
+      "target": 25
+    },
+    {
+      "source": 11,
+      "target": 28
+    },
+    {
+      "source": 11,
+      "target": 29
+    },
+    {
+      "source": 11,
+      "target": 32
+    },
+    {
+      "source": 11,
+      "target": 45
+    },
+    {
+      "source": 11,
+      "target": 46
+    },
+    {
+      "source": 11,
+      "target": 47
+    },
+    {
+      "source": 11,
+      "target": 48
+    },
+    {
+      "source": 11,
+      "target": 50
+    }
+  ]
+};
